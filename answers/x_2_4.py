39844334c27759c3bdb77329e1a58b2114725160
Shin Morichika <ofl@users.noreply.github.com>
2022-10-29 13:39:26 +0900

    fix 2-5

--- answers/x_2_4.py
+++ answers/x_2_4.py
@@ -1,21 +1,28 @@
 # x_2_4
 #
-# count_downから１つずつ数字を引いて10から5までの数字を表示してください
+# count_downから１つずつ数字を引いて
+# 「10」
+# 「9」
+# 「8」
+# 「7」
+# 「6」
+# 「5」
+# と表示してください
 
 count_down = 10
 print(count_down)
 
-count_down -= 1
+count_down = count_down - 1
 print(count_down)
 
-count_down -= 1
+count_down = count_down - 1
 print(count_down)
 
-count_down -= 1
+count_down = count_down - 1
 print(count_down)
 
-count_down -= 1
+count_down = count_down - 1
 print(count_down)
 
-count_down -= 1
+count_down = count_down - 1
 print(count_down)
