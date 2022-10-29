50c4d2ff070586973de92bb6b0b9b1dd11223914
Shin Morichika <ofl@users.noreply.github.com>
2022-10-29 11:13:37 +0900

    fix 8-9

--- x_8_9.py
+++ x_8_9.py
@@ -6,6 +6,6 @@ cards = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 
 print(cards)
 
-cards[2], cards[11] = cards[11], cards[2]
+cards[2], cards[11] = cards[11], cards[2]  # cards[2]とcards[11]の要素を交換
 
 print(cards)
