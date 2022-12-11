e9d006a664245239bd33d39df1774484c86dbbc6
hinahina-chips <hinahina-chips@outlook.jp>
2022-12-11 19:03:28 +0900

    [6-7]桃の部分の削除とヒントの追加

--- answers/x_6_7.py
+++ answers/x_6_7.py
@@ -1,10 +1,10 @@
 # x_6_7
 #
-# 「while文」を使って以下のように表示してください(鬼の真ん中に桃が１文字含まれています)
+# 「while文」を追加して以下のように表示してください
 #
 # 鬼鬼鬼鬼鬼
 # 鬼鬼鬼鬼鬼
-# 鬼鬼桃鬼鬼
+# 鬼鬼鬼鬼鬼
 # 鬼鬼鬼鬼鬼
 # 鬼鬼鬼鬼鬼
 
@@ -13,10 +13,7 @@ while gyou < 5:
     retu = 0
     text = ''
     while retu < 5:
-        if gyou == 2 and retu == 2:
-            text += '桃'
-        else:
-            text += '鬼'
+        text += '鬼'
         retu += 1
     print(text)
     gyou += 1
