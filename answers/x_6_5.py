385886e289cf8aa8cc92f3815cc457a2b872509d
hinahina-chips <hinahina-chips@outlook.jp>
2023-01-06 18:58:42 +0900

    insertを使うように修正

--- answers/x_6_5.py
+++ answers/x_6_5.py
@@ -1,7 +1,7 @@
 # x_6_5
 #
-# 「members」が最後にどんな値となるかを予想してください
 # 「members」の値を確認したら、append、remove、insertを使って「members」を元に戻してください
+# 'さる'を追加する時はappendは使わないようにしてください
 
 members = ['桃太郎', 'いぬ', 'さる', 'きじ']
 
@@ -23,9 +23,9 @@ members.append('かに')
 
 members.remove('うらしまたろう')
 members.remove('きんたろう')
+members.insert(2, 'さる')
 members.remove('うす')
 members.remove('かに')
-members.append('さる')
 members.append('きじ')
 
 print(members)
