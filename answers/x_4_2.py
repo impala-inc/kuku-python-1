6008be1ac2ed0ad55d3bdca70dac7a00cae21da8
Shin Morichika <ofl@covered.jp>
2023-01-07 12:59:19 +0900

    [4-2]問題を修正

--- answers/x_4_2.py
+++ answers/x_4_2.py
@@ -1,14 +1,30 @@
 # x_4_2
 #
 # 「4-1」で使った繰り返し処理の構文を「while文」と呼びます
-# 「number」が偶数の時は「○は偶数です」と奇数の時「○は奇数です」と表示されるように修正してください
+# 九九の9の段「9,18,27,,,,81」を表示するように修正してください
+
+number = 9  # 初期値
+
+while number <= 81:
+    print(number)
+
+    number += 9
+
+# こちらの書き方でも問題ありません
 
 number = 1  # 初期値
 
-while number <= 10:
-    if number % 2 == 0:
-        print(str(number) + 'は偶数です')
-    else:
-        print(str(number) + 'は奇数です')
+while number < 10:
+    print(number * 9)
+
+    number += 1
+
+# [応用問題]
+# 「9 X 1 = 9」「9 X 2 = 18」のように表示させてみてください
+
+number = 1  # 初期値
+
+while number < 10:
+    print(9, 'X', number, '=', number * 9)
 
     number += 1
