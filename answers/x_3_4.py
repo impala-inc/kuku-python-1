47c2286b43bc94e9311f7fe0adee8e7843f8c5d9
Shin Morichika <ofl@covered.jp>
2023-01-21 15:29:13 +0900

    [3-4] 問題の追加と解答コメントの追加と__name__を削除した

--- answers/x_3_4.py
+++ answers/x_3_4.py
@@ -4,14 +4,15 @@
 
 # ヒント
 
-hint_1 = type(True).__name__
-hint_2 = type(False).__name__
+hint_1 = type(True)
+hint_2 = type(False)
 hint_3 = int(True)
 hint_4 = int(False)
 
 # ここから問題
 
-q_1 = bool(1)      # => True
-q_2 = bool(0)      # => False
-q_3 = bool('abc')  # => True
-q_4 = bool('')     # => False(空文字「''」はFalseと判定される)
+q_1 = bool(1)      # => True(「0」以外の数字はTrueと判定される)
+q_2 = bool(-100)   # => True(「0」以外の数字はTrueと判定される)
+q_3 = bool(0)      # => False(「0」はFalseと判定される)
+q_4 = bool('abc')  # => True(空文字「''」以外はTrueと判定される)
+q_5 = bool('')     # => False(空文字「''」はFalseと判定される)
--- x_3_4.py
+++ x_3_4.py
@@ -6,17 +6,18 @@ from module import qa
 
 # ヒント
 
-hint_1 = type(True).__name__
-hint_2 = type(False).__name__
+hint_1 = type(True)
+hint_2 = type(False)
 hint_3 = int(True)
 hint_4 = int(False)
 
 # ここから問題
 
 q_1 = bool(1)
-q_2 = bool(0)
-q_3 = bool('abc')
-q_4 = bool('')
+q_2 = bool(-100)
+q_3 = bool(0)
+q_4 = bool('abc')
+q_5 = bool('')
 
 
 # ここはとりあえず無視
