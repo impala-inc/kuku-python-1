fe7e7cd6d2d21d2e83ce50d55a198c398c3d3f03
hinahina-chips <hinahina-chips@outlook.jp>
2023-01-27 14:54:37 +0900

    ヒントを追加

--- x_9_3.py
+++ x_9_3.py
@@ -1,8 +1,11 @@
 # x_9_3
 #
 # 「7-9」で入力した質問と回答を「chatbot.csv」に保存するように修正してください
+# 前後でファイルの中身を確認してください
 
 import csv
+
+# ここで質問と回答を入力する
 
 file = open('./files/chatbot.csv', mode='a', encoding="utf-8")
 
