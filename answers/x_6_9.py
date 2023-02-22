942b72b82de32568442b8fb5b80a8dc72cadc670
hinahina-chips <hinahina-chips@outlook.jp>
2023-02-22 19:02:09 +0900

    [6-9]問題を簡単にした

--- answers/x_6_9.py
+++ answers/x_6_9.py
@@ -1,22 +1,16 @@
 # x_6_9
 #
 # while文を使って次のリストを数が小さい順に並び替えてください
-# ヒント: 隣り合った数字を比べて「右」の数字が「左」の数字よりも大きければ「右」と「左」の数字を入れ替えます
-# ヒント: numbers[0], numbers[1] = numbers[1], numbers[0] で１番目と２番目の数字を入れ替えることができます
+# ヒント: min()とremove()を使ってnumbersの最小値を求めていくと簡単です
 
 numbers = [70, 29, 23, 12, 77, 55, 29, 62]
+new_numbers = []
 
 while True:
-    i = 0
-    is_changed = False
-
-    while i < len(numbers) - 1:
-        if numbers[i] > numbers[i + 1]:
-            numbers[i], numbers[i + 1] = numbers[i + 1], numbers[i]
-            is_changed = True
-        i += 1
-
-    if not is_changed:
+    if not numbers:
         break
 
-print(numbers)
+    new_numbers.append(min(numbers))
+    numbers.remove(min(numbers))
+
+print(new_numbers)
